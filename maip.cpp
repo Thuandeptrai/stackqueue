342b1d6ee838e939f03a371c51901bf13414a69a
thuan <tvc4567@gmail.com>
2022-04-08 08:47:46 +0700

    prettier

--- maip.cpp
+++ maip.cpp
@@ -1,194 +1,146 @@
-
 #include<bits/stdc++.h>
+
 using namespace std;
-int Ans[100000];
+int Ans[1000000];
 int numb = 0;
 
-struct Node
-{
-    int data;
-    Node* link;
+struct Node {
+  int data;
+  Node * link;
 };
 
-Node* top;
+Node * top;
 
+void push(int data) {
 
-void push(int data)
-{
+  Node * temp = new Node();
 
+  if (!temp) {
 
-    Node* temp = new Node();
+    exit(1);
+  }
 
+  temp -> data = data;
 
-    if (!temp)
-    {
+  temp -> link = top;
 
-        exit(1);
-    }
-
-
-    temp->data = data;
-
-    temp->link = top;
-
-
-    top = temp;
+  top = temp;
 }
 
+int isEmpty() {
 
-int isEmpty()
-{
-
-    return top == NULL;
+  return top == NULL;
 }
 
+int peek() {
 
-int peek()
-{
-
-
-    if (!isEmpty())
-        return top->data;
-    else
-        exit(1);
+  if (!isEmpty())
+    return top -> data;
+  else
+    exit(1);
 }
 
+void pop() {
+  Node * temp;
 
-void pop()
-{
-    Node* temp;
+  if (top == NULL) {
 
+    exit(1);
+  } else {
 
-    if (top == NULL)
-    {
+    temp = top;
 
-        exit(1);
-    }
-    else
-    {
+    top = top -> link;
 
-
-        temp = top;
-
-
-        top = top->link;
-
-
-
-        free(temp);
-    }
+    free(temp);
+  }
 }
 
-
- struct QNode {
-    int data;
-    QNode* next;
-    QNode(int d)
-    {
-        data = d;
-        next = NULL;
-    }
+struct QNode {
+  int data;
+  QNode * next;
+  QNode(int d) {
+    data = d;
+    next = NULL;
+  }
 };
 
 struct Queue {
-    QNode *fronts, *rear;
-    Queue()
-    {
-        fronts = rear = NULL;
+  QNode * fronts, * rear;
+  Queue() {
+    fronts = rear = NULL;
+  }
+
+  void enQueue(int x) {
+
+    QNode * temp = new QNode(x);
+
+    if (rear == NULL) {
+      fronts = rear = temp;
+      return;
     }
 
-    void enQueue(int x)
-    {
+    rear -> next = temp;
+    rear = temp;
+  }
 
-        QNode* temp = new QNode(x);
+  void deQueue() {
 
+    if (fronts == NULL)
+      return;
 
-        if (rear == NULL) {
-            fronts = rear = temp;
-            return;
-        }
+    QNode * temp = fronts;
+    fronts = fronts -> next;
 
+    if (fronts == NULL)
+      rear = NULL;
 
-        rear->next = temp;
-        rear = temp;
-    }
-
-    void deQueue()
-    {
-
-        if (fronts == NULL)
-            return;
-
-        QNode* temp = fronts;
-        fronts = fronts->next;
-
-
-        if (fronts == NULL)
-            rear = NULL;
-
-        delete (temp);
-    }
+    delete(temp);
+  }
 };
 
+void Ispalimore(string letters) {
 
-void Ispalimore(string letters)
-{
+  Queue q;
+  int a = 0, temp = 0;
+  for (int i = 0; i < letters.size(); i++) {
+    int Inttest = (int) letters[i] - 48;
 
+    q.enQueue(Inttest);
+    push(Inttest);
 
-        Queue q;
-        int a = 0;
-        for(int i = 0; i < letters.size(); i++)
-        {
-                int Inttest = (int)letters[i] - 48;
+  }
 
-                      q.enQueue(Inttest);
-        push(Inttest);
+  while (a != letters.size() && (q.fronts) -> data == peek()) {
+    a++;
+    pop();
+    q.deQueue();
+  }
 
-        }
+  if (a == letters.size()) {
 
+    if (stoi(letters) % 2 != 0) {
 
-        while (a != letters.size()&& (q.fronts)->data == peek() )
-        {
-            a++;
-           pop();
-           q.deQueue();
-        }
+      numb++;
+      Ans[numb] = stoi(letters);
+    }
 
-        if ( a== letters.size()){
-
-            if( stoi(letters) % 2 != 0)
-            {
-
-            numb++;
-                Ans[numb] = stoi(letters);
-            }
-
-        }
-
-
-
-
+  }
 
 }
 
+int main() {
+  int n;
+  cin >> n;
+  for (int i = 0; i < n; i++) {
+    auto s = std::to_string(i);
+    Ispalimore(s);
 
-int main()
-{
-int n;
-    cin >> n;
-    for(int i = 0; i < n; i++)
-    {
-stringstream ss;
-ss << i;
-string str = ss.str();
-    Ispalimore(str);
+  }
 
-    }
+  for (int i = 1; i <= numb; i++) {
+    cout << Ans[i] << " ";
+  }
 
-     for(int i = 1; i <= numb; i++)
-     {
-       cout << Ans[i] << " ";
-     }
-
-    return 0;
+  return 0;
 }
